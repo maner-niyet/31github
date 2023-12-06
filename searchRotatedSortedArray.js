ec6017aba17f25b93354b3130fa48940d4c19219
Maner Niyet <manerniyet15@gmail.com>
2023-12-05 23:18:54 -0800

    add searchRotatedSortedArray.js

---
+++ searchRotatedSortedArray.js
@@ -0,0 +1,21 @@
+const search = (nums, target) => {
+    let left = 0; right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if (target === nums[mid]) return mid;
+        if (nums[left] <= nums[mid]) {
+            if (target > nums[mid] || target < nums[left]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } else {
+            if (target < nums[mid] || target > nums[right]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+    return -1;
+};
